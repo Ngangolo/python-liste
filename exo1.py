a9e98626ed2b495dfb6337aedd8d119927a21317
NGANGOLO <konengangolo54@gmail.com>
2021-02-19 13:51:38 +0100

    exo 1 liste

---
+++ exo1.py
@@ -0,0 +1,10 @@
+jours=["lundi","mardi","mercredi","jeudi","vendredi"]
+chiffres=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+composite=[0, "chocolat", 145, "Python", "Landais", 12.1, True]
+print(chiffres) ; print(composite) ; print(jours[0]) ; print(jours[1]) ; print(jours[4]) ;
+print(composite[2]) ; print(chiffres[3]+composite[2]) ; print(chiffres[9]*composite[0]) ;
+print(jours[0] + jours[2])
+if composite[6] :
+ print(composite[3] + " " + composite[4])
+if not composite[6] :
+ print("Pas la peine de réfléchir plus longtemps !")
